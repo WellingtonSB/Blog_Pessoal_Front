e8147ba135cc13a1d481e0b577dc6e9366e41afd
Wellington <wellingtonsouza2504@gmail.com>
2021-04-30 11:46:54 -0300

    teste

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,11 @@
+export class UserLogin
+{
+    
+    public nome: string
+    public usuario:string
+    public foto:string
+    public tipo:string
+    public senha:string
+    public token: string
+ 
+}
