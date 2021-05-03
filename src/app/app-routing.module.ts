0f47fb21925a9af2c16d0792d9c8b4daae66a862
Wellington <wellingtonsouza2504@gmail.com>
2021-05-03 16:12:40 -0300

    add tema

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
+import { TemaComponent } from './tema/tema.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 
 const routes: Routes = [
 
@@ -11,8 +11,10 @@ const routes: Routes = [
 
   {path:'entrar', component: EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent},
+
   {path: 'inicio', component: InicioComponent},
-  {path: 'menu',component:MenuComponent}
+  {path: 'tema', component: TemaComponent}
+
 ];
 
 @NgModule({
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +21,16 @@ export class AuthService {
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
+
+  logado(){
+    let ok = false //variavel booleana(let ok:boolean = false)
+    if(environment.token !=''){
+        ok= true
+    }
+    
+    return ok
+  }
+
+
+
 }
