698ded15a094c6398bf1a4675404a68fd0a3187b
Wellington <wellingtonsouza2504@gmail.com>
2021-05-10 14:07:26 -0300

    teste v2

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,10 @@
+  
+import { environment } from './../../environments/environment.prod';
 import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
-import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -22,15 +23,19 @@ export class AuthService {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 
-  logado(){
-    let ok = false //variavel booleana(let ok:boolean = false)
-    if(environment.token !=''){
-        ok= true
-    }
-    
-    return ok
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
   }
 
 
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
+  }
 
 }
